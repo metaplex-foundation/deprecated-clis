bda7207024fca136bd8568d20aa33ee35c1c73d6
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-06-28 17:03:11 -0500

    first commit

---
+++ src/execute.sh
@@ -0,0 +1,12 @@
+#ts-node auction-house-cli.ts execute_sale -e mainnet-beta -k ~/.config/solana/id.json -ah 7aZEV7xJRYJ9R33A8m55mv8dg9BxU6h4V4d7djRvNnsA -t 1 -m BHCVzNJ59ML3X7z3BewKop4y9mmUsR8X7ufvzTeHvwMy -bw zAHR1epxLNXCM5R4n249eo6W5J9chWnuMPusyp96Ajr -sw 9hThpeFugp1e4hZYG4XCPmWeVpuPZHKazZ5CCfELmuxi -b 0.4
+ts-node auction-house-cli.ts execute_sale -e mainnet-beta -k ~/.config/solana/id.json -ah 7aZEV7xJRYJ9R33A8m55mv8dg9BxU6h4V4d7djRvNnsA -t 1 -m 29mTrZ1xaV6GVuzg8xvKbmJXa9ZJftNLyJ5AsYt9wFfR -bw D6wFNaDssqnhbgonM9w9PGVKKdmi6sNER1Y3oFcYzyL7 -sw DvmtnKYCzKN5UmXcXuDN34Lx6oxXNv6ZXobD6ESypMFL -b 5 -ak ~/.config/solana/id.json
+#ts-node auction-house-cli.ts execute_sale -e mainnet-beta -k ~/.config/solana/id.json -ah 7aZEV7xJRYJ9R33A8m55mv8dg9BxU6h4V4d7djRvNnsA -t 1 -m -bw -sw -b
+
+#$ ts-node src/auction-house-cli.ts execute_sale
+#  -k ~/mywallet.key \
+#  -ah HsKwc8dQtm8KLxshw67dwsNePkH6wMXo5Lwn1FuKjVYVS \
+#  --buy-price 2 \
+#  --mint DCqt9QQ3ot3qv53EhWrYAWFuh4XgSvFJvLRjgsDnhLTp \
+#  --buyer-wallet 3DikCrEsfAVHv9rXENg2Hdmc16L71EjveQEF4NbSfRak \
+#  --seller-wallet CCJC2s8FDGAs8GqmngE9gviusEuNnkdUwchcYMZ8ZmHB \
+#  --token-size 1
